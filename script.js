05fd69f6c8ae1e9bb92461e117f229632147fb14
Shah Ehsan <shahehsan2002@gmail.com>
2025-12-02 14:30:17 +0600

    explore global,functio, block scope

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var name = "John";
 let age = 30;
 const country = "USA";  
 console.log(name, age, country);
+
+
+// Scope (global, function, block)
+var x = 10; // Global scope
+function myFunction() {
+    let y = 20; // Function scope
+    if (true) {
+        var z = 30; // Block scope
+    }
+}
